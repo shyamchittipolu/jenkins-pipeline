e1c781b7667979133239e88aaee442bae9157139
shyam <shyam@gmail.com>
2025-01-25 21:40:45 -0500

    bucketnamechanged

--- 1-terraform-jenkins-server/backend.tf
+++ 1-terraform-jenkins-server/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-app-kub-2024-v2"
+    bucket = "jenkins-app-kub-2024-v2-5"
     region = "eu-west-3"
     key = "jenkins-server/terraform.tfstate"
   }
